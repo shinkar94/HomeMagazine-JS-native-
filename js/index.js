f360b50967dda057e9f4fd490d1e1dc675586ba2
shinkar94 <romka94shinkarenko@gmail.com>
2023-01-15 15:52:32 +0300

    add onclick title

--- js/index.js
+++ js/index.js
@@ -12,15 +12,31 @@ addDataState(STATE)
 dataOperation.forEach(e => {
     // отправка данных
     let btnSend = e.querySelector(".btnSend");
-    btnSend.addEventListener('click',() => {
-        let date = e.querySelector(".dateSend").value;
-        let opirationName = e.querySelector(".opirationName").value;
-        let sum = e.querySelector(".sum").value;
-        let description = e.querySelector(".description").value;
-        let statusSend = btnSend.classList[1];
-    })
+    if(btnSend){
+        btnSend.addEventListener('click',() => {
+            let date = e.querySelector(".dateSend").value;
+            let opirationName = e.querySelector(".opirationName").value;
+            let sum = e.querySelector(".sum").value;
+            let description = e.querySelector(".description").value;
+            let statusSend = btnSend.classList[1];
+        })
+    }
 });
 
 function updateTable(){
     // добавление tr в конец таблицы
 }
+
+// анимация блока DATA
+const TIT_DATE = document.querySelectorAll(".tittleData");
+const DATA_BLOCK = document.querySelectorAll(".Data");
+const INFO_BLOCK= document.querySelectorAll(".Info");
+
+function animTitDat(dataB, infoB){  
+    TIT_DATE.forEach(tit => {
+        tit.addEventListener('click', ()=>{
+            console.log("click title");
+        });
+    });
+}
+animTitDat(DATA_BLOCK, INFO_BLOCK);
