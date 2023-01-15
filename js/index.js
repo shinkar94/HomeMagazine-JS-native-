a1fa310d0fbb01908ceb40ab44d85a05458fe9d7
shinkar94 <romka94shinkarenko@gmail.com>
2023-01-15 14:59:44 +0300

    update js file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-console.log("connect")
+const dataOperation = document.querySelectorAll(".Data");
+
+const STATE = {
+    money: 273
+}
+
+function addDataState(STATE){
+    console.log(STATE.money);
+}
+addDataState(STATE)
+
+dataOperation.forEach(e => {
+    // отправка данных
+    let btnSend = e.querySelector(".btnSend");
+    btnSend.addEventListener('click',() => {
+        let date = e.querySelector(".dateSend").value;
+        let opirationName = e.querySelector(".opirationName").value;
+        let sum = e.querySelector(".sum").value;
+        let description = e.querySelector(".description").value;
+        let statusSend = btnSend.classList[1];
+    })
+});
+
+function updateTable(){
+    // добавление tr в конец таблицы
+}
